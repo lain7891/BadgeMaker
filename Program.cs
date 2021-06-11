0a9f9a63391bfe0073f5905f45bc20260dc9c3d2
Estefania <nialupo87@gmail.com>
2021-06-11 12:31:24 -0400

    Badge project

---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic; 
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+         static List<Employee> GetEmployees(){
+
+       List<Employee> employees = new List<Employee>();
+       
+while (true) 
+{
+Console.WriteLine("Please enter a name: ");
+string firstName = Console.ReadLine();
+if(firstName == ""){
+    break;
+}
+// Console.Write("Enter first name: ");
+// string firstName = Console.ReadLine();
+Console.Write("Enter last name: ");
+string lastName = Console.ReadLine();
+Console.Write("Enter ID: ");
+int id = Int32.Parse(Console.ReadLine());
+Console.Write("Enter Photo URL: ");
+string photoUrl = Console.ReadLine();
+
+
+Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+employees.Add(currentEmployee);
+
+// for(int i = 0; i < employees.Count; i++){
+//     Console.WriteLine(employees[i]);
+// };
+
+};
+ return employees;
+
+         }
+
+        // static void PrintEmployees(List<Employee> employees){
+           
+        // }
+    
+      
+
+
+//          }
+
+// Console.WriteLine("My employees include {0}, {1}, {2}, {3}", employees[0], employees[1], employees[2], employees[3]);
+       
+        
+
+        static void Main(string[] args)
+        {
+
+            List<Employee> employees = GetEmployees();
+            // PrintEmployees(employees);
+                 Util.PrintEmployees(employees);
+                 Util.MakeCsv(employees);
+                 Util.MakeBadges(employees);
+
+        }
+
+       
+
+//             for(int i = 0; i < employees.Count; i++){
+//     Console.WriteLine(employees[i]);
+// };
+            // Console.WriteLine("Hello World!");
+            // string greeting = "Hello";
+            // greeting = greeting + "World";
+            // Console.WriteLine("greeting: {0}", greeting);
+
+            // double side = 3.14;
+            // double area = side * side;
+            // // Console.WriteLine($"area {area}");
+            // Console.WriteLine("area is a {0}", area.GetType());
+
+        // string stringNum = "2";
+        // int intNum = Convert.ToInt16(stringNum);
+        // Console.WriteLine(intNum);
+        // Console.WriteLine(intNum.GetType());
+
+        // Dictionary<string, int> myScoreBoard = new Dictionary<string, int>(){
+        //     {"firstInning", 10},
+        //      {"secondInning", 20},
+        //       {"thirdInning", 30},
+        //        {"fourthInning", 40},
+        //         {"fifthInning", 50},
+
+        // };
+        // Console.WriteLine("------------------");
+        // Console.WriteLine(" Scoreboeard");
+        // Console.WriteLine("------------------");
+        // Console.WriteLine("Inning | Score");
+        // Console.WriteLine(" 1 | {0}", myScoreBoard["firstInning"]);
+        //  Console.WriteLine(" 2 | {0}", myScoreBoard["secondInning"]);
+        //   Console.WriteLine(" 3 | {0}", myScoreBoard["thirdInning"]);
+        //    Console.WriteLine(" 4 | {0}", myScoreBoard["fourthInning"]);
+        //     Console.WriteLine(" 5 | {0}", myScoreBoard["fifthInning"]);
+
+        // 
+       
+     
+    
+}
+
+}
---
+++ Util.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic; 
+using System.IO;
+using System.Drawing; 
+using System.Net;
+
+
+namespace CatWorx.BadgeMaker
+{
+    class Util 
+    {
+        public static void PrintEmployees(List<Employee> employees){
+             for(int i = 0; i < employees.Count; i++){
+                string template = "{0,-10}\t{1, -20}\t{2}";
+  Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
+            }
+        }
+
+        public static void MakeCsv(List<Employee> employees){
+            if (!Directory.Exists("data")){
+                Directory.CreateDirectory("data");
+            };
+              using (StreamWriter file = new StreamWriter("data/employees.csv")){
+            file.WriteLine("ID, Name,PhotoUrl");
+            for(int i = 0; i < employees.Count; i++){
+                string template = "{0}, {1}, {2}";
+                file.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
+            }
+        }
+        }
+
+      
+      public static void MakeBadges(List<Employee> employees){
+        //    Create Image
+        Image newImage = Image.FromFile("badge.png");
+        newImage.Save("data/employeeBadge.png");
+
+        int BADGE_WIDTH = 669;
+        int BADGE_HEIGHT = 1044;
+
+        int COMPANY_NAME_START_X = 0;
+            int COMPANY_NAME_START_Y = 110;
+                int COMPANY_NAME_WIDTH = 100;
+
+                int PHOTO_START_X = 184;
+                int PHOTO_START_Y = 215;
+                int PHOTO_WIDTH = 302;
+                int PHOTO_HEIGHT = 302;
+
+                int EMPLOYEE_NAME_START_X = 0;
+                int EMPLOYEE_NAME_START_Y = 0;
+                int EMPLOYEE_NAME_WIDTH = BADGE_WIDTH;
+                int EMPLOYEE_NAME_HEIGHT = 100;
+
+                int EMPLOYEE_ID_START_X = 0;
+                int EMPLOYEE_ID_START_Y = 690;
+                int EMPLOYEE_ID_WIDTH = BADGE_WIDTH;
+                int EMPLOYEE_ID_HEIGHT = 100;
+
+
+      }
+
+    
+  WebClient client = new WebClient();
+  using(WebClient client = new WebClient()){
+      for(int i = 0; i < employees.Count; i++)
+      {
+          
+      }
+)
+  }
+
+    }
+}
